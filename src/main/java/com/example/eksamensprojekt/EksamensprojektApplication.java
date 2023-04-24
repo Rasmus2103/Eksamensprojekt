6bd1f051600fa26614948d3d061c5feef33c6cb4
Rasmus2103 <rasm110c@stud.kea.dk>
2023-04-24 12:44:00 +0200

    Commit

---
+++ src/main/java/com/example/eksamensprojekt/EksamensprojektApplication.java
@@ -0,0 +1,13 @@
+package com.example.eksamensprojekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EksamensprojektApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EksamensprojektApplication.class, args);
+    }
+
+}
