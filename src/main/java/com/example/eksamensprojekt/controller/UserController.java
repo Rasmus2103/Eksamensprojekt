25022393dce70c9d8fc46c9dfcc97d969fecf763
Rasmus2103 <rasm110c@stud.kea.dk>
2023-04-24 13:39:18 +0200

    Commit

---
+++ src/main/java/com/example/eksamensprojekt/controller/UserController.java
@@ -0,0 +1,8 @@
+package com.example.eksamensprojekt.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class UserController {
+
+}
---
+++ src/main/java/com/example/eksamensprojekt/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.example.eksamensprojekt.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserRepository {
+
+}
