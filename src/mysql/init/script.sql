576a8c36375bd38508071db55b4352fc6a274638
jonathanFS14 <jona08p8@stud.kea.dk>
2023-05-02 11:26:28 +0200

    updated db script

--- src/mysql/init/script.sql
+++ src/mysql/init/script.sql
@@ -4,13 +4,53 @@ USE eksamensprojekt;
 
 CREATE TABLE user (
 userid INT auto_increment,
-username VARCHAR(50),
+name VARCHAR(30),
+username VARCHAR(50) UNIQUE,
 userpassword VARCHAR(50),
 PRIMARY KEY (userid)
 );
 
-INSERT INTO user (username, userpassword) values
-("martin123", "login123"),
-("ulrik123", "login123");
+CREATE TABLE project (
+projectid INT auto_increment,
+projectname VARCHAR(50),
+PRIMARY KEY (projectid),
+);
+
+CREATE TABLE board (
+boardid INT auto_increment,
+boardname VARCHAR(50),
+projectid INT,
+PRIMARY KEY (boardid),
+FOREIGN KEY (projectid) REFERENCES project (projectid)
+);
+
+CREATE TABLE story (
+storyid INT auto_increment,
+storyname VARCHAR(50),
+storydescription VARCHAR(500),
+acceptcriteria VARCHAR(500),
+deadline DATE,
+boardid INT,
+PRIMARY KEY (storyid),
+FOREIGN KEY (boardid) REFERENCES board (boardid)
+);
+
+CREATE TABLE task (
+taskid INT auto_increment,
+taskname VARCHAR(50),
+taskdescription VARCHAR(500),
+storypoints INT,
+storyid INT,
+PRIMARY KEY (taskid),
+FOREIGN KEY (storyid) REFERENCES story (storyid)
+);
+
+CREATE TABLE userproject (
+userid INT,
+projectid INT,
+PRIMARY KEY (userid, projectid)
+FOREIGN KEY (userid) REFERENCES user (userid)
+FOREIGN KEY (projectid) REFERENCES project (projectid)
+);
 
 commit;
