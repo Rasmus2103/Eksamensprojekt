feaad01be09ee6eb4008a85e3607f518373d3120
jonathanFS14 <jona08p8@stud.kea.dk>
2023-05-02 12:36:44 +0200

    ....

--- src/main/java/com/example/eksamensprojekt/controllers/PMController.java
+++ src/main/java/com/example/eksamensprojekt/controllers/PMController.java
@@ -1,20 +1,17 @@
 package com.example.eksamensprojekt.controllers;
 
-import com.example.eksamensprojekt.model.User;
 import com.example.eksamensprojekt.repository.RepositoryDB;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.List;
-
 @Controller
 @RequestMapping("")
-public class UserController {
+public class PMController {
     private RepositoryDB repositoryDB;
 
-    public UserController(RepositoryDB repositoryDB) {
+    public PMController(RepositoryDB repositoryDB) {
         this.repositoryDB = repositoryDB;
     }
 
