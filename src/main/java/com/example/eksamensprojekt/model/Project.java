75bd00f45ecc5afe79a564e418e9f212b7775745
jonathanFS14 <jona08p8@stud.kea.dk>
2023-05-02 11:44:35 +0200

    model classes

--- src/main/java/com/example/eksamensprojekt/model/Project.java
+++ src/main/java/com/example/eksamensprojekt/model/Project.java
@@ -1,4 +1,33 @@
 package com.example.eksamensprojekt.model;
 
 public class Project {
+
+    private int projectid;
+    private String projectname;
+
+    public Project(int projectid, String projectname){
+        this.projectid = projectid;
+        this.projectname = projectname;
+    }
+
+    public Project(){
+
+    }
+
+    public int getProjectid() {
+        return projectid;
+    }
+
+    public void setProjectid(int projectid) {
+        this.projectid = projectid;
+    }
+
+    public String getProjectname() {
+        return projectname;
+    }
+
+    public void setProjectname(String projectname) {
+        this.projectname = projectname;
+    }
+
 }
