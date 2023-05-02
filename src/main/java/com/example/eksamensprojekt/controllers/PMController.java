65e314cf969115c87ec06c069b24f80fcc7bec44
Rasmus2103 <rasm110c@stud.kea.dk>
2023-05-02 15:07:41 +0200

    Lavet noget i Controller og ændret lidt i html sider.

--- src/main/java/com/example/eksamensprojekt/controllers/PMController.java
+++ src/main/java/com/example/eksamensprojekt/controllers/PMController.java
@@ -1,13 +1,20 @@
 package com.example.eksamensprojekt.controllers;
 
+import com.example.eksamensprojekt.model.Project;
+import com.example.eksamensprojekt.model.User;
 import com.example.eksamensprojekt.repository.RepositoryDB;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
-@RequestMapping("")
+@RequestMapping("/")
 public class PMController {
     private RepositoryDB repositoryDB;
 
@@ -15,10 +22,56 @@ public class PMController {
         this.repositoryDB = repositoryDB;
     }
 
-    @GetMapping("/")
-    public String getUsers(Model model) {
-      //List<User> users = repositoryDB.getUsers();
-      //model.addAttribute("user", users);
-      return "index";
+    private boolean isLogged(HttpSession session) {
+        User user = (User) session.getAttribute("user");
+        return user != null;
     }
+
+    @GetMapping("login")
+    public String login(@RequestParam("username") String username, @RequestParam("password") String password, HttpSession session, Model model) {
+        User user = repositoryDB.getUser(repositoryDB.getUserid(username));
+        if(user != null && user.getPassword().equals(password)) {
+            session.setAttribute("user", user);
+            return "redirect:/userProjects" + user.getUserid();
+        }
+        model.addAttribute("wrongCredentials", true);
+        return "index";
+    }
+
+    @GetMapping("logout")
+    public String logout(HttpSession session) {
+        // invalidate session and return landing page
+        session.invalidate();
+        return "redirect:/projectManagement";
+    }
+
+    @GetMapping("userProjects/{id}")
+    public String getUserProjects(@PathVariable("id") int userid, Model model, HttpSession session) {
+        if(isLogged(session)) {
+            User user = (User) session.getAttribute("user");
+            if (user.getUserid() == userid) {
+                model.addAttribute("user", user);
+
+                List<Project> projects = repositoryDB.getProjects(userid);
+                model.addAttribute("projects", projects);
+                return "userProjects";
+            } else {
+                return "redirect:/projectManagement";
+            }
+        } else {
+            return "index";
+        }
+    }
+
+    @GetMapping("project/{userid}/{projectid}")
+    public String getProject(@PathVariable("userid") int userid, @PathVariable("projectid") int projectid, Model model, HttpSession session) {
+        User user = repositoryDB.getUser(userid);
+        model.addAttribute("user", user);
+
+        Project project = repositoryDB.getSpecificProject(projectid);
+        model.addAttribute("project", project);
+
+        return isLogged(session) ? "project" : "index";
+    }
+
 }
