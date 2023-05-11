beb7516e7330ab4294a1607ed6c5fc64fb31da2d
jonathanFS14 <jona08p8@stud.kea.dk>
2023-05-11 12:58:50 +0200

    added sessions.get(userid) to all methods so that the nav bar home butten works

--- src/main/java/com/example/eksamensprojekt/controllers/PMController.java
+++ src/main/java/com/example/eksamensprojekt/controllers/PMController.java
@@ -133,23 +133,18 @@ public class PMController {
     }
 
     @GetMapping("/project/{projectId}/{userId}/addusers")
-    public String showAddUserForm(@PathVariable("projectId") int projectId, @PathVariable("userId") int userId, Model model) {
+    public String showAddUserForm(@PathVariable("projectId") int projectId, @PathVariable("userId") int userId, Model model, HttpSession session) {
         Project project = repositoryDB.getSpecificProject(projectId);
         List<User> users = repositoryDB.getAllUsers();
+
+        Object userid = session.getAttribute("userid");
+        model.addAttribute("userid", userid);
 
         model.addAttribute("project", project);
         model.addAttribute("users", users);
 
         return "addusers";
     }
-
-    /*@PostMapping("/project/{projectId}/adduser")
-    public String addUserToProject(@PathVariable("projectId") int projectId, @ModelAttribute("users") List<User> users) {
-        // TODO metoden virker ikke
-        for(int userId: )
-        repositoryDB.addUserToProject(users.indexOf(0), projectId);
-        return "redirect:/project/" + projectId;
-    }*/
 
     @PostMapping("/project/{projectId}/{userId}/adduser")
     public String addUserToProject(@PathVariable("projectId") int projectId, @PathVariable("userId") int userId, @RequestParam("userIds") List<Integer> userIds) {
@@ -192,6 +187,9 @@ public class PMController {
         List<Story> stories = repositoryDB.getStories(boardid);
         model.addAttribute("stories", stories);
 
+        Object userid = session.getAttribute("userid");
+        model.addAttribute("userid", userid);
+
         return isLogged(session) ? "storylist" : "index";
     }
 
@@ -202,6 +200,9 @@ public class PMController {
 
         Board board = repositoryDB.getSpecificBoard(boardid);
         model.addAttribute("board", board);
+
+        Object userid = session.getAttribute("userid");
+        model.addAttribute("userid", userid);
 
         return isLogged(session) ? "createstory" : "index";
     }
@@ -229,6 +230,9 @@ public class PMController {
         List<Task> tasks = repositoryDB.getTasks(storyid);
         model.addAttribute("tasks", tasks);
 
+        Object userid = session.getAttribute("userid");
+        model.addAttribute("userid", userid);
+
         int totalStoryPoints = repositoryDB.getSumOfStoryPointsForBoard(storyid);
         model.addAttribute("totalStoryPoints", totalStoryPoints);
 
@@ -239,6 +243,8 @@ public class PMController {
     public String updateStory(@PathVariable("storyid") int storyid, Model model, HttpSession session) {
         Story story = repositoryDB.getSpecificStory(storyid);
         model.addAttribute("story", story);
+        Object userid = session.getAttribute("userid");
+        model.addAttribute("userid", userid);
         return isLogged(session) ? "updatestory" : "index";
     }
 
@@ -297,6 +303,9 @@ public class PMController {
         Task task1 = repositoryDB.getSpecificTask(storyid);
         model.addAttribute("task1", task1);
 
+        Object userid = session.getAttribute("userid");
+        model.addAttribute("userid", userid);
+
         return isLogged(session) ? "createtask" : "index";
     }
 
@@ -310,6 +319,8 @@ public class PMController {
     public String updateTask(@PathVariable("taskid") int taskid, Model model, HttpSession session) {
         Task task = repositoryDB.getSpecificTask(taskid);
         model.addAttribute("task", task);
+        Object userid = session.getAttribute("userid");
+        model.addAttribute("userid", userid);
         return isLogged(session) ? "updatetask" : "index";
     }
 
