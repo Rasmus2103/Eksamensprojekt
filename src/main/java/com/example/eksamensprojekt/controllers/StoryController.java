b789fac1d15e88ff323d67767f953cf79e766388
Rasmus2103 <rasm110c@stud.kea.dk>
2023-05-17 14:00:59 +0200

    Lavet nogen ting

--- src/main/java/com/example/eksamensprojekt/controllers/StoryController.java
+++ src/main/java/com/example/eksamensprojekt/controllers/StoryController.java
@@ -80,6 +80,23 @@ public String getStories(@PathVariable("boardid") int boardid, Model model, Http
         return "redirect:/storylist/" + currentBoard.getBoardid();
     }
 
+    @GetMapping("moveStoryBackToSprintBoard/{storyId}")
+    public String moveHistoryStoryToSprintBoard(@PathVariable("storyId") int storyId) {
+        // First, find the current board and project associated with the story.
+        Story story = storyRepository.getSpecificStory(storyId);
+        Board currentBoard = boardRepository.getSpecificBoard(story.getBoardid());
+        int projectId = currentBoard.getProjectid();
+
+        // Now, find the backlog board id for the corresponding project.
+        int backlogBoardId = boardRepository.getBacklogBoardIdByProjectId(projectId);
+
+        // Move the story to the backlog board.
+        storyRepository.moveStoryToBoard(storyId, backlogBoardId);
+
+        // Redirect back to the story list for the original board.
+        return "redirect:/storylist/" + currentBoard.getBoardid();
+    }
+
 
 
     @GetMapping("story/createstory/{boardid}")
